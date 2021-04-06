a27d0ce6b5fa25e5748d5b774ca9d02aa8891fc9
Aleksey <alekseyhonchar@gmail.com>
2021-04-06 12:37:28 +0300

    Готова форма авторизації

--- CCS/CCS/Forms/auth.Designer.cs
+++ CCS/CCS/Forms/auth.Designer.cs
@@ -30,73 +30,112 @@ namespace CCS
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(auth));
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAuth = new System.Windows.Forms.Button();
+            this.forgotLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.wrongLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonAuth
             // 
-            this.button1.Location = new System.Drawing.Point(134, 147);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Авторизуватися";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonAuth.BackColor = System.Drawing.Color.Black;
+            this.buttonAuth.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonAuth.FlatAppearance.BorderSize = 0;
+            this.buttonAuth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAuth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAuth.ForeColor = System.Drawing.Color.White;
+            this.buttonAuth.Location = new System.Drawing.Point(134, 147);
+            this.buttonAuth.Name = "buttonAuth";
+            this.buttonAuth.Size = new System.Drawing.Size(148, 44);
+            this.buttonAuth.TabIndex = 0;
+            this.buttonAuth.Text = "Авторизуватися";
+            this.buttonAuth.UseVisualStyleBackColor = false;
+            this.buttonAuth.Click += new System.EventHandler(this.buttonAuth_Click);
             // 
-            // label1
+            // forgotLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(152, 194);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Забули логін/пароль?";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.forgotLabel.AutoSize = true;
+            this.forgotLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.forgotLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.forgotLabel.ForeColor = System.Drawing.Color.White;
+            this.forgotLabel.Location = new System.Drawing.Point(131, 194);
+            this.forgotLabel.Name = "forgotLabel";
+            this.forgotLabel.Size = new System.Drawing.Size(151, 16);
+            this.forgotLabel.TabIndex = 1;
+            this.forgotLabel.Text = "Забули логін/пароль?";
+            this.forgotLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.forgotLabel.Click += new System.EventHandler(this.forgotLabel_Click);
+            this.forgotLabel.MouseLeave += new System.EventHandler(this.forgotLabel_MouseLeave);
+            this.forgotLabel.MouseHover += new System.EventHandler(this.forgotLabel_MouseHover);
             // 
-            // textBox1
+            // loginTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(109, 85);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Логін";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.loginTextBox.ForeColor = System.Drawing.Color.DimGray;
+            this.loginTextBox.Location = new System.Drawing.Point(109, 85);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(206, 20);
+            this.loginTextBox.TabIndex = 2;
+            this.loginTextBox.Text = "prettyEmail@gmail.com";
+            this.loginTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.loginTextBox.Click += new System.EventHandler(this.loginTextBox_Click);
+            this.loginTextBox.TextChanged += new System.EventHandler(this.loginTextBox_TextChanged);
+            this.loginTextBox.Leave += new System.EventHandler(this.loginTextBox_Leave);
             // 
-            // textBox2
+            // passwordTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(109, 111);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(206, 20);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Пароль";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passwordTextBox.ForeColor = System.Drawing.Color.DimGray;
+            this.passwordTextBox.Location = new System.Drawing.Point(109, 111);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(206, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.Text = "prettyStrongPassword";
+            this.passwordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passwordTextBox.Click += new System.EventHandler(this.passwordTextBox_Click);
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            this.passwordTextBox.Leave += new System.EventHandler(this.passwordTextBox_Leave);
             // 
-            // label2
+            // nameLabel
             // 
-            this.label2.Location = new System.Drawing.Point(194, 35);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Олег";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.nameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameLabel.ForeColor = System.Drawing.Color.White;
+            this.nameLabel.Location = new System.Drawing.Point(29, 9);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(367, 38);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "Система клімат-контролю";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // wrongLabel
+            // 
+            this.wrongLabel.AutoSize = true;
+            this.wrongLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wrongLabel.ForeColor = System.Drawing.Color.Firebrick;
+            this.wrongLabel.Location = new System.Drawing.Point(55, 228);
+            this.wrongLabel.Name = "wrongLabel";
+            this.wrongLabel.Size = new System.Drawing.Size(317, 16);
+            this.wrongLabel.TabIndex = 5;
+            this.wrongLabel.Text = "Ви ввели неправильний логін або пароль";
+            this.wrongLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // auth
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Gray;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(424, 216);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(424, 253);
+            this.Controls.Add(this.wrongLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.forgotLabel);
+            this.Controls.Add(this.buttonAuth);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "auth";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Авторизація";
             this.Load += new System.EventHandler(this.auth_Load);
             this.ResumeLayout(false);
@@ -106,11 +145,12 @@ namespace CCS
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAuth;
+        private System.Windows.Forms.Label forgotLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label wrongLabel;
     }
 }
 
--- CCS/CCS/Forms/auth.cs
+++ CCS/CCS/Forms/auth.cs
@@ -12,16 +12,87 @@ namespace CCS
 {
     public partial class auth : Form
     {
+        
         public auth()
         {
             InitializeComponent();
-            
         }
-
 
         private void auth_Load(object sender, EventArgs e)
         {
+            this.BackColor = ColorTranslator.FromHtml("#0396A6");
+            buttonAuth.BackColor = ColorTranslator.FromHtml("#026873");
+            wrongLabel.ForeColor = ColorTranslator.FromHtml("#0D0D0D");
+            wrongLabel.Visible = false;
+        }
 
+        private void forgotLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void forgotLabel_MouseHover(object sender, EventArgs e)
+        {
+            forgotLabel.Font = new Font(forgotLabel.Font, FontStyle.Underline);
+        }
+
+        private void forgotLabel_MouseLeave(object sender, EventArgs e)
+        {
+            forgotLabel.Font = new Font(forgotLabel.Font, FontStyle.Regular);
+        }
+
+        private void buttonAuth_Click(object sender, EventArgs e)
+        {
+            wrongLabel.Visible = true;
+        }
+
+        private void loginTextBox_Click(object sender, EventArgs e)
+        {
+            loginTextBox.SelectAll();
+            if (loginTextBox.Text == "prettyEmail@gmail.com")
+                loginTextBox.Text = "";
+        }
+
+        private void passwordTextBox_Click(object sender, EventArgs e)
+        {
+            passwordTextBox.SelectAll();
+            if (loginTextBox.Text == "prettyStrongPassword")
+                passwordTextBox.Text = "";
+        }
+
+        private void loginTextBox_TextChanged(object sender, EventArgs e)
+        {
+            loginTextBox.ForeColor = ColorTranslator.FromHtml("#0D0D0D");
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            passwordTextBox.ForeColor = ColorTranslator.FromHtml("#0D0D0D");
+        }
+
+        private void CheckTextBox(TextBox textbox)
+        {
+            if (textbox.Text.Length == 0)
+            {
+                if (textbox.Name == "loginTextBox")
+                    textbox.Text = "prettyEmail@gmail.com";
+                if (textbox.Name == "passwordTextBox")
+                    textbox.Text = "prettyStrongPassword";
+            }
+            if (textbox.Text == "prettyEmail@gmail.com")
+                textbox.ForeColor = Color.DimGray;
+            if (textbox.Text == "prettyStrongPassword")
+                textbox.ForeColor = Color.DimGray;
+        }
+
+        private void loginTextBox_Leave(object sender, EventArgs e)
+        {
+            CheckTextBox(loginTextBox);
+        }
+
+        private void passwordTextBox_Leave(object sender, EventArgs e)
+        {
+            CheckTextBox(passwordTextBox);
         }
     }
 }
