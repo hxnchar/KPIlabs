8dcd9836c5cda4a657f07f023942cf390f1bcbe5
Hluushchenko <hluushchenko@gmail.com>
2021-04-06 10:57:57 +0300

    main form added

---
+++ CCS/CCS/Forms/mainForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace CCS
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
