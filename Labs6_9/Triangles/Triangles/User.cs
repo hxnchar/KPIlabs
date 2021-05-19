fd902bcdc6cfe23f6677134c4f6fb2422b597ed8
Aleksey <alekseyhonchar@gmail.com>
2021-05-20 00:08:16 +0300

    BEST USER CONSTRUCTOR ADDED

--- Labs6_9/Triangles/Triangles/User.cs
+++ Labs6_9/Triangles/Triangles/User.cs
@@ -8,12 +8,35 @@ namespace Triangles
         private bool _isAdmin;
         public string Name;
 
-        public User(string login, string password)
+        public User(bool registration, string login, string password)
         {
-            using (var sr = new StreamReader("D:/courses/users.bd"))
+            Name = login;
+            if (registration)
             {
-                
+                using (var sw = File.AppendText("D:/courses/users.bd"))
+                {
+                    sw.WriteLine($"{login},{password},0");
+                }
+                _auth = true;
             }
+            else
+            {
+                using (var sr = new StreamReader("D:/courses/users.bd"))
+                {
+                    string currentString;
+                    while ((currentString = sr.ReadLine()) !=null)
+                    {
+                        string[] parsedString = currentString.Split(",");
+                        if (parsedString[0] == login && parsedString[1] == password)
+                        {
+                            if (parsedString[2] == "1") _isAdmin = true;
+                            else _isAdmin = false;
+                            _auth = true;
+                        }
+                    }
+                }
+            }
+            
         }
     }
 }
